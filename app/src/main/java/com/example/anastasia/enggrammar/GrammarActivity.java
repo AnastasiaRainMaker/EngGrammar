5c1defb319987a66051c4e526159b8231897db37
Anastasia <humaine@yandex.ru>
2017-12-24 20:11:06 +0300

    first commit

---
+++ app/src/main/java/com/example/anastasia/enggrammar/GrammarActivity.java
@@ -0,0 +1,13 @@
+package com.example.anastasia.enggrammar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GrammarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grammar);
+    }
+}
